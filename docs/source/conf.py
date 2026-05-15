0f53cd9a01526f5deeeda84c8aceaa187b027d23
TabDynasty <118533454+TabDynasty@users.noreply.github.com>
2026-05-15 12:29:08 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
+project = 'ReadmyBlog'
 copyright = '2021, Graziella'
-author = 'Graziella'
+author = 'tabDynasty'
 
 release = '0.1'
 version = '0.1.0'
